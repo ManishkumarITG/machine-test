ac8333d9294548e7ff5afdf310d7a2efb7f0dcca
ManishKumarITG <manishkumar@itgeeks.com>
2025-09-29 19:01:01 +0530

    form valid in one click

--- script.js
+++ script.js
@@ -1,63 +1,68 @@
 let inputs = document.querySelectorAll('input');
 let p = Array.from(document.querySelectorAll('form p:not([class])'));
 let submissionBTN = document.querySelector('form');
+let dataArr = [];
 function firstName() {
     if (inputs[0].value.length > 3) {
         p[0].style.display = 'none'
-        return true
-    }
+        dataArr.push(true)
+       
+    }else{
     p[0].style.display = 'block'
     p[0].style.color = "red"
-    return false;
+    return false;}
 }
 console.log(p[0].innerHTML);
 
 function lastName(params) {
     if (inputs[1].value.length > 3) {
-         p[1].style.display = 'none'
-        return true
-    }
+        p[1].style.display = 'none'
+        dataArr.push(true)
+     
+    }else{
     p[1].style.display = 'block'
     p[1].style.color = "red"
-    return false;
+    return false;}
 }
 function email() {
     if (inputs[2].value.length > 4) {
-         p[2].style.display = 'none'
-        return true
-    }
+        p[2].style.display = 'none'
+        dataArr.push(true)
+      
+    }else{
     p[2].style.display = 'block'
     p[2].style.color = "red"
-    return false;
+    dataArr.push(true)
+    return false;}
 }
 function number() {
     if (inputs[3].value.length == 10) {
-         p[3].style.display = 'none'
-        return true
-    }
+        p[3].style.display = 'none'
+        dataArr.push(true)
+    }else{
     p[3].style.display = 'block'
     p[3].style.color = "red"
-    return false;
+    return false;}
 }
 
 
 function dob() {
     if (inputs[4].value) {
-         p[4].style.display = 'none'
-        return true
-    }
+        p[4].style.display = 'none'
+        dataArr.push(true)
+    }else{
     p[4].style.display = 'block'
     p[4].style.color = "red"
-    return false;
+    return false;}
 }
 function gender() {
     if (inputs[5].checked || inputs[6].checked || inputs[7].checked) {
-         p[5].style.display = 'none'
-        return true
-    }
+        p[5].style.display = 'none'
+        dataArr.push(true)
+    }else{
     p[5].style.display = 'block'
     p[5].style.color = "red"
-    return false;
+    return false;}
 }
 let arr = []
 function hobbei() {
@@ -67,54 +72,54 @@ function hobbei() {
         if (inputs[10].checked) { arr.push('reading books') }
         if (inputs[11].checked) { arr.push('riding bike') }
         if (inputs[12].checked) { arr.push('other') }
-         p[6].style.display = 'none'
-        return true
-    }
+        p[6].style.display = 'none'
+        dataArr.push(true)
+    }else{
     p[6].style.display = 'block'
     p[6].style.color = "red"
-    return false;
+    return false;}
 }
 function message() {
     if ((document.querySelector('textarea').value.length > 5)) {
-         p[7].style.display = 'none'
-        return true;
-    }
+        p[7].style.display = 'none'
+        dataArr.push(true);
+    }else{
     p[7].style.display = 'block'
     p[7].style.color = "red"
-    return false;
+    return false;}
 }
 function checkOne() {
     if (inputs[13].checked) {
-         p[8].style.display = 'none'
-        return true
-    }
+        p[8].style.display = 'none'
+        dataArr.push(true)
+    }else{
     p[8].style.display = 'block'
     p[8].style.color = "red"
-    return false;
+    return false;}
 }
 function checkTwo() {
     if (inputs[14].checked) {
         p[9].style.display = 'none'
-        return true
-    }
+        dataArr.push(true)
+    }else{
     p[9].style.display = 'block'
     p[9].style.color = "red"
-    return false;
+    return false;}
 }
 function checkThree() {
-
     if (inputs[15].checked) {
-         p[10].style.display = 'none'
-        return true
-    }
+        p[10].style.display = 'none'
+        dataArr.push(true)
+    }else{
     p[10].style.display = 'block'
     p[10].style.color = "red"
-    return false;
+    return false;}
 }
 submissionBTN.addEventListener('submit', (e) => {
+    console.log(dataArr.length)
+    firstName() , lastName(), gender() , email() , message() , dob() , checkOne() , checkTwo() ,checkThree() , number() , hobbei()
     e.preventDefault()
-    if (firstName() && lastName() && gender()  && email() &&
-        message() && dob() && checkOne() && checkTwo() && checkThree() && number() && hobbei()) {
+    if (dataArr.length == 11) {
         console.log('First Name', inputs[0].value)
         console.log('Second Name', inputs[1].value)
         console.log('email', inputs[2].value)
@@ -129,12 +134,14 @@ submissionBTN.addEventListener('submit', (e) => {
         }
         console.log('Hobbies', arr)
         console.log('message', document.querySelector('textarea').value)
-        arr = []
+        dataArr=[]
     }
+    arr = []
+    dataArr=[]
 })
 
 let dropDownOnhumburger = document.querySelector(".dropDownOnhumburger")
-function droperDown (){
-   dropDownOnhumburger.classList.toggle('active');
-   console.log(dropDownOnhumburger);  
+function droperDown() {
+    dropDownOnhumburger.classList.toggle('active');
+    console.log(dropDownOnhumburger);
 }
